bdd3c5830997809d30a77304f20b2633075f3968
Amit P Naik <amit.naik8103@gmail.com>
2020-03-24 17:51:58 +0530

    Factory design pattern

---
+++ DesignPattern/FactoryDesignPattern.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+
+
+    public enum ObjectType
+    {
+        Customer,
+        Supplier
+    }
+
+    class CreateFactory
+    {
+        public static IFactory GetObject(ObjectType type)
+        {
+            IFactory factory = null;
+
+            switch (type)
+            {
+                case ObjectType.Customer:
+                    factory = new CustomerFactory();
+                    break;
+
+                case ObjectType.Supplier:
+                    factory = new SupplierFactory();
+                    break;
+
+                default:
+                    throw new NotSupportedException();
+                    
+            }
+
+            return factory;
+        }
+    }
+
+    interface IFactory
+    {
+        string getName();
+    }
+
+    class CustomerFactory : IFactory
+    {
+       
+        public string getName()
+        {
+            return "Customer name";
+        }
+    }
+
+    class SupplierFactory : IFactory
+    {
+       
+
+        public string getName()
+        {
+            return "Supplier name";
+        }
+    }
+}
---
+++ DesignPattern/SimpleFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+
+    public interface ICustomer
+    {
+        void CalculateBill();
+    }
+
+    public class GoldCustomer : ICustomer
+    {
+        public void CalculateBill()
+        {
+            Console.WriteLine("Gold customer - calculate bill");
+        }
+    }
+
+    public class SilverCustomer : ICustomer
+    {
+        public void CalculateBill()
+        {
+            Console.WriteLine("Silver customer - calculate bill");
+        }
+    }
+
+
+    public static class SimpleFactory
+    {
+        public static ICustomer Create(int i)
+        {
+            if (i == 0)
+            {
+                return new GoldCustomer();
+            }
+            else
+            {
+                return new SilverCustomer();
+            }
+        }
+    }
+}
